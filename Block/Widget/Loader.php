7d97d7cce53285f98670ca814633e03753adf3d5
Yaroslav Garmash <garmashdev@gmail.com>
2020-02-26 17:13:53 +0200

    Initial commit

---
+++ Block/Widget/Loader.php
@@ -0,0 +1,10 @@
+<?php
+namespace Atom\Loader\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class Loader extends Template implements BlockInterface
+{
+
+}
